dfb54b996b319b88c06d5b6f9f71a0d38a4bf9fc
nyxeet <31820971+nyxeet@users.noreply.github.com>
2020-10-29 21:16:13 +0200

    Update task3.js

--- js/task3.js
+++ js/task3.js
@@ -1,4 +1,13 @@
 const findLongestWord = function (string) {
+  let longestWord = '';
   const stringArray = string.split(' ');
-  for (let i = 0; i < stringArray.length; i += 1) {}
+  for (let i = 0; i < stringArray.length; i += 1) {
+    if (stringArray[i].length > longestWord.length) {
+      longestWord = stringArray[i];
+    }
+  }
+  return longestWord;
 };
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog'));
+console.log(findLongestWord('Google do a roll'));
+console.log(findLongestWord('May the force be with you')); // 'force'
