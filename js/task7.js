d6fb484f420a0932204d02f5c4201e003c2cf54b
nyxeet <31820971+nyxeet@users.noreply.github.com>
2020-11-01 17:36:33 +0200

    sa

---
+++ js/task7.js
@@ -0,0 +1,38 @@
+const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
+
+const isLoginValid = function (login) {
+  if (login.length >= 4 && login.length <= 16) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const isLoginUnique = function (allLogins, login) {
+  console.log(login);
+  if (allLogins.includes(login)) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const addLogin = function (allLogins, login) {
+  if (isLoginValid(login) === false) {
+    return 'Ошибка! Логин должен быть от 4 до 16 символов';
+  }
+  if (isLoginUnique(allLogins, login) === false) {
+    return 'Такой логин уже используется!';
+  } else {
+    allLogins.push(login);
+    return 'Логин успешно добавлен!';
+  }
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(addLogin(logins, 'Ajax')); // 'Логин успешно добавлен!'
+console.log(addLogin(logins, 'robotGoogles')); // 'Такой логин уже используется!'
+console.log(addLogin(logins, 'Zod')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+console.log(addLogin(logins, 'jqueryisextremelyfast')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
